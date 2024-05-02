d3bf71877720950d3268712ccc891fd12a77cd44
george <george.dobric@gmail.com>
2024-05-02 18:41:28 -0400

    Hierarchy logic.

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,68 @@
-import logo from './logo.svg';
-import { createSignal, createEffect, For } from "solid-js";
-import './App.css'
+import logo from "./logo.svg";
+import { createSignal, createEffect, For, onMount, onCleanup } from "solid-js";
+import "./App.css";
+import { createScrollPosition } from "@solid-primitives/scroll";
 
 function App() {
-
   //  Signals
 
-  const [branch, setBranch] = createSignal([{id: [1,1], hierarchy: [1], subject: "First Branch"},{id: [1,2], hierarchy: [1], subject: "Second Branch"}])
-  const [jobs,setJobs] = createSignal([{id: 1, title: "1st Job", tree:[branch()]}]);
-  const [inputValue, setInputValue] = createSignal('');
-  const [searchInputValue, setSearchInputValue] = createSignal('');
+  const [branch, setBranch] = createSignal([
+    { id: [1, 1], hierarchy: [1], subject: "First Branch" },
+    { id: [1, 2], hierarchy: [1], subject: "Second Branch" },
+  ]);
+  const [jobs, setJobs] = createSignal([
+    { id: 1, title: "1st Job", tree: [branch()] },
+  ]);
+  const [inputValue, setInputValue] = createSignal("");
+  const [searchInputValue, setSearchInputValue] = createSignal("");
   const [currentJob, setCurrentJob] = createSignal(1);
   const [currentHierarchy, setCurrentHierarchy] = createSignal([1]);
+  const [mouseOverID, setMouseOverID] = createSignal();
+  const [hierarchy, setHierarchy] = createSignal([1]);
+  const windowScroll = createScrollPosition();
+  console.log(branch()[0].hierarchy.toString());
+  console.log(hierarchy().toString());
+  console.log(hierarchy().toString() == branch()[0].hierarchy.toString());
 
   //  Handle main input, creating a new branch which will appear on the screen
   const handleKeyDown = (e) => {
-    if (e.key === 'Enter') {
-
+    if (e.key === "Enter") {
       let newBranch = {
-        id: [],
-        hierarchy: [],
-        subject: inputValue()
+        id: [...hierarchy(), jobs()[currentJob() - 1].tree[0].length],
+        hierarchy: hierarchy(),
+        subject: inputValue(),
       };
-      let updatedTree = [...jobs()[currentJob()-1].tree[0], newBranch];
+      let updatedTree = [...jobs()[currentJob() - 1].tree[0], newBranch];
       const updatedJobs = jobs();
-      updatedJobs[currentJob()-1].tree[0] = updatedTree;
+      updatedJobs[currentJob() - 1].tree[0] = updatedTree;
       setJobs(updatedJobs);
 
       // Clear the input value
-      setInputValue('');
+      setInputValue("");
 
       // update the DOM
-      setBranch(jobs()[currentJob()-1].tree[0]);
+      setBranch(jobs()[currentJob() - 1].tree[0]);
     }
-  }
+  };
 
   //  Add Job Button Styling
   const [addButtonStyle, setAddButtonStyle] = createSignal("addButton");
   const handleMouseDown = () => {
     setAddButtonStyle("addButtonPressed");
-  }
+  };
   const handleMouseUp = () => {
     setAddButtonStyle("addButton");
-  }
+  };
 
   //  Add a new job
   const handleAddJob = () => {
     let title = prompt("Project Title: ");
-    let branchSubject = prompt("First thing: ")
-    let branch = [{id: [1,1], hierarchy: [1], subject: branchSubject}]
+    let branchSubject = prompt("First thing: ");
+    let branch = [{ id: [1, 1], hierarchy: [1], subject: branchSubject }];
     const newJob = {
-      id: jobs().length + 1, 
+      id: jobs().length + 1,
       title: title,
-      tree: [branch]
+      tree: [branch],
     };
     let updatedJobs = jobs();
     updatedJobs = [...updatedJobs, newJob];
@@ -62,59 +72,113 @@ function App() {
     console.log(currentJob());
 
     // update the DOM
-    setBranch(jobs()[currentJob()-1].tree[0]);
-  }
+    setBranch(jobs()[currentJob() - 1].tree[0]);
+  };
 
-  const handleMouseEnter = (id) => {
-    // console.log(id)
-    console.log('ENTER');
-  }
-  const handleMouseLeave = (id) => {
-    // console.log(id)
-    console.log('LEAVE');
-  }
+  const [branchHover, setBranchHover] = createSignal(false);
+  const handleMouseEnter = (branch) => {
+    setMouseOverID(branch.id);
+    console.log(mouseOverID());
+    setBranchHover(true);
+  };
+  const handleMouseLeave = () => {
+    setMouseOverID();
+    setBranchHover(false);
+  };
+
+  const [shiftComplete, setShiftComplete] = createSignal(false);
+
+  const handleHierarchyShift = () => {
+    if (shiftComplete() == false) {
+      if (wDelta() == "up" && branchHover() == true) {
+        console.log("scroll up");
+        console.log(mouseOverID());
+        setShiftComplete(true);
+        setHierarchy(mouseOverID);
+      } else if (wDelta() == "down" && branchHover() == true) {
+        console.log("scroll down");
+        setShiftComplete(true);
+      }
+      setWDelta("");
+    }
+  };
+
+  const [wDelta, setWDelta] = createSignal("");
+  // const [scrolleRegistered, setScrollRegistered] = createSignal(false);
+
+  const handleMouseWheel = (e) => {
+    const delta = e.deltaY;
+    let direction = delta < 0 ? "up" : "down";
+    if (delta == 0) direction = "";
+
+    setWDelta(direction);
+  };
+
+  // Add event listener when the component is mounted
+  onMount(() => {
+    window.addEventListener("wheel", handleMouseWheel);
+    // Remove event listener when the component is unmounted
+    onCleanup(() => {
+      window.removeEventListener("wheel", handleMouseWheel);
+    });
+  });
 
   return (
     <div>
-
       <input
         class="mainInput"
         value={inputValue()}
         onInput={(e) => setInputValue(e.target.value)}
-        onKeyDown={handleKeyDown}></input>
+        onKeyDown={handleKeyDown}
+      ></input>
 
-      <button class={addButtonStyle()}
+      <button
+        class={addButtonStyle()}
         onMouseDown={handleMouseDown}
         onMouseUp={handleMouseUp}
-        onClick={handleAddJob}>➕</button>
+        onClick={handleAddJob}
+      >
+        ➕
+      </button>
 
       <div class="nodeContainer">
-
-        <For each={jobs()}>{(job, i) =>
-        
-          <li
-          // onMouseEnter={handleMouseEnter(job.tree[i].id)}
-          class="list"
-          onMouseEnter={handleMouseEnter}
-          onMouseLeave={handleMouseLeave}>
-            {currentJob() == job.id &&
-            <For each={branch()}>{(branch, j) =>
-              <div class="node"
-              >{branch.subject}</div>
-            }</For>
-          }
-          </li>
-        }</For>
+        <For each={jobs()}>
+          {(job, i) => (
+            <li
+              // onMouseEnter={handleMouseEnter(job.tree[i].id)}
+              class="list"
+            >
+              {currentJob() == job.id && (
+                <For each={branch()}>
+                  {(branch, j) => (
+                    <>
+                      {branch.hierarchy.toString() ==
+                        hierarchy().toString() && (
+                        <div
+                          class="node"
+                          onMouseEnter={() => handleMouseEnter(branch)}
+                          onMouseLeave={handleMouseLeave}
+                          onWheel={handleHierarchyShift}
+                        >
+                          {branch.subject}
+                        </div>
+                      )}
+                    </>
+                  )}
+                </For>
+              )}
+            </li>
+          )}
+        </For>
 
         <input
           class="searchContainer"
           placeholder="🔎 Search 🔎"
           value={searchInputValue()}
           onInput={(e) => setSearchInputValue(e.target.value)}
-          onKeyDown={handleKeyDown}></input>
+          onKeyDown={handleKeyDown}
+        ></input>
       </div>
-    
-    
     </div>
   );
 }
